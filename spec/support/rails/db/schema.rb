3a7452af450f30b79e0a56630890ee3783c4bf43
Dale Stevens <dale@getnotion.com>
2017-11-28 17:04:19 -0700

    We don't need to test against UUID, since that's a bit specialized; :bigserial should suffice.

--- spec/support/rails/db/schema.rb
+++ spec/support/rails/db/schema.rb
@@ -18,13 +18,13 @@ ActiveRecord::Schema.define do
     t.integer :god_powers
   end
 
-  create_table :posts, id: :uuid, force: true do |t|
+  create_table :posts, id: :bigserial, force: true do |t|
     t.integer :user_id
     t.string :title
     t.timestamps null: false
   end
 
-  create_table :post_tags, id: :uuid, default: nil, primary_key: :post_id, force: true do |t|
+  create_table :post_tags, id: :bigserial, default: nil, primary_key: :post_id, force: true do |t|
     t.string :title
     t.timestamps null: false
   end
