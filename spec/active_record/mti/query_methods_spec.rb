b79304038c29bca08eb83cb121cb005ebdd70ea2
Dale Stevens <dale@getnotion.com>
2017-12-14 13:59:17 -0700

    Additional query around general tableoid selection on "normal" queries

---
+++ spec/active_record/mti/query_methods_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe ActiveRecord::MTI::QueryMethods do
+
+  context "queries" do
+    it "select tableoid" do
+      sql = Admin.all.to_sql
+      expect(sql).to match(/SELECT .*, \"admins\".\"tableoid\" AS tableoid FROM \"admins\"/)
+    end
+  end
+
+end
