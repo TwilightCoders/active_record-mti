f3955c056fdef4c1332375f24c1233be6c13d1cc
Dale Stevens <dale@twilightcoders.net>
2017-12-30 13:11:22 -0700

    Move tests for views

--- spec/active_record/mti/inheritance_spec.rb
+++ spec/active_record/mti/inheritance_spec.rb
@@ -102,24 +102,11 @@ describe ActiveRecord::MTI::Inheritance do
     end
   end
 
-  describe 'views' do
-    before(:each) do
 
-      User.connection.execute <<-SQL
-        CREATE OR REPLACE VIEW "users_all"
-        AS SELECT * FROM "users"
-      SQL
 
-      class UserView < User
-        self.table_name = "users_all"
-      end
 
-    end
 
-    if ActiveRecord::Base.connection.version >= Gem::Version.new('9.4')
-      it 'allows creation pass-through' do
 
-        UserView.create(email: 'dale@twilightcoders.net')
       end
     end
   end
