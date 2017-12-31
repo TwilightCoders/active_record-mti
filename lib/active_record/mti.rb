b08f7dea1ddd1a4db553367990a5f09056daf65c
Yuki Nishijima <yk.nishijima@gmail.com>
2017-12-31 11:59:16 -0500

    Add #child_tables and #parent_tables methods

--- lib/active_record/mti.rb
+++ lib/active_record/mti.rb
@@ -44,5 +44,35 @@ module ActiveRecord
     def self.testify(value)
       value == true || value == 't' || value == 1 || value == '1'
     end
+
+    def self.child_tables
+      @child_tables ||= ActiveRecord::Base.connection.execute(SQL_FOR_CHILD_TABLES).to_a.map do |row|
+                          ChildTable.new(*row.values).freeze
+                        end.freeze
+    end
+
+    def self.parent_tables
+      @parent_tables ||= ActiveRecord::Base.connection.execute(SQL_FOR_PARENT_TABLES).to_a.map do |row|
+                           ParentTable.new(*row.values).freeze
+                         end.freeze
+    end
+
+    ChildTable  = Struct.new(:inhrelid, :inhparent, :inhseqno, :oid, :name, :parent_table_name)
+    ParentTable = Struct.new(:oid, :name)
+
+    SQL_FOR_CHILD_TABLES = <<-SQL.gsub(/\s+/, " ").strip
+      SELECT "pg_inherits".*, "child".oid AS oid, "child".relname AS name, "parent".relname AS parent_table_name
+        FROM "pg_inherits"
+        JOIN "pg_class" AS "child" ON ("child".oid = "pg_inherits".inhrelid)
+        JOIN "pg_class" AS "parent" ON ("parent".oid = "pg_inherits".inhparent)
+    SQL
+
+    SQL_FOR_PARENT_TABLES = <<-SQL.gsub(/\s+/, " ").strip
+      SELECT DISTINCT("pg_class".oid) AS oid, "pg_class".relname AS name
+        FROM "pg_class", "pg_inherits"
+        WHERE "pg_class".oid = "pg_inherits".inhparent
+    SQL
+
+    private_constant :ChildTable, :ParentTable, :SQL_FOR_CHILD_TABLES, :SQL_FOR_PARENT_TABLES
   end
 end
--- spec/active_record/mti_spec.rb
+++ spec/active_record/mti_spec.rb
@@ -7,5 +7,34 @@ describe ActiveRecord::MTI do
         expect(ActiveRecord::MTI.testify('f')).to eq(false)
       end
     end
+
+  end
+
+  describe "#child_tables" do
+    it "returns an array of child tables" do
+      # Sort the tables by name to not depend on the order
+      child_tables = ActiveRecord::MTI.child_tables.sort_by(&:name)
+
+      expect(child_tables[0].name).to eq('admin/hackers')
+      expect(child_tables[0].parent_table_name).to eq('admins')
+
+      expect(child_tables[1].name).to eq('admins')
+      expect(child_tables[1].parent_table_name).to eq('users')
+
+      expect(child_tables[2].name).to eq('vehicles/trucks')
+      expect(child_tables[2].parent_table_name).to eq('vehicles')
+    end
+  end
+
+  describe "#parent_tables" do
+    it "returns an array of child tables" do
+      # Sort the tables by name to not depend on the order
+      parent_tables = ActiveRecord::MTI.parent_tables.sort_by(&:name)
+
+      # binding.pry
+      expect(parent_tables[0].name).to eq('admins')
+      expect(parent_tables[1].name).to eq('users')
+      expect(parent_tables[2].name).to eq('vehicles')
+    end
   end
 end
